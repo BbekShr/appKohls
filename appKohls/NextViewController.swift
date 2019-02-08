2bcee092d05b4d496b65f954634c6ac009c8543d
Bibek <bbeksh@gmail.com>
2019-02-07 19:29:26 -0600

    Initial Commit

---
+++ appKohls/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  nextViewController.swift
+//  appKohls
+//
+//  Created by Bibek Shrestha on 2/7/19.
+//  Copyright © 2019 Bibek Shrestha. All rights reserved.
+//
+
+
+import UIKit
+
+class NextViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        let title_icon = UIImage(named: "KohlsLogo.png")
+        let titleImageView = UIImageView(image: title_icon)
+        
+        navigationItem.titleView = titleImageView
+        titleImageView.frame = CGRect(x:0, y: 0, width:10, height:100 )
+        titleImageView.contentMode = .scaleAspectFit
+        
+    }
+    
+    
+}
